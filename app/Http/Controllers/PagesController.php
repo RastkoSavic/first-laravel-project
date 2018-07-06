6c5e2bbc9e9671121fd62bb8080eb40efab47ff4
RastkoSavic <rastkosavic23@gmail.com>
2018-07-06 02:01:28 +0200

    Templating with Blade

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,7 +6,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $title = 'Welcome to      Laravel!';
+        $title = 'Welcome to Laravel!';
         // return view('pages.index', compact('title'));
         return view('pages.index')->with('title', $title);
 
@@ -21,7 +21,12 @@ class PagesController extends Controller
 
     public function services()
     {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+
+        return view('pages.services')->with($data);
     }
 
 }
